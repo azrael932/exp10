92ec9ac575ab22778984b6b0638987ef17a3fe6c
azrael932 <aryanzsoji@gmail.com>
2025-10-30 00:27:50 +0530

    Update external_led_blink.ino

--- external_led_blink.ino
+++ external_led_blink.ino
@@ -2,26 +2,27 @@
 #include <std_msgs/Empty.h>
 
 ros::NodeHandle nh;
+const int LED_PIN = 13;
+bool should_blink = false;
 
-const int led_pin = 8; // External LED connected to pin 8
-bool led_state = LOW;
-
-void messageCb(const std_msgs::Empty& toggle_msg){
-  led_state = !led_state;
-  digitalWrite(led_pin, led_state);
+void startBlinkCallback(const std_msgs::Empty& msg) {
+  should_blink = true;
 }
 
-ros::Subscriber<std_msgs::Empty> sub("toggle_external_led", &messageCb);
+ros::Subscriber<std_msgs::Empty> blink_sub("start_blink", &startBlinkCallback);
 
 void setup() {
-  pinMode(led_pin, OUTPUT);
-  digitalWrite(led_pin, led_state);
-  
+  pinMode(LED_PIN, OUTPUT);
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(blink_sub);
 }
 
 void loop() {
+  if (should_blink) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+  }
   nh.spinOnce();
-  delay(100);
 }
